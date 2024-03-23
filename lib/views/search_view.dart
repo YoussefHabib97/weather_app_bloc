c39247073ddd848da9bad8712c2c44696bcca1be
Youssef <ykmhabib97@gmail.com>
2024-03-23 19:52:43 +0200

    Implemented getWeather in search view with exception handling

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:weather_app_bloc/models/weather_model.dart';
+import 'package:weather_app_bloc/services/weather_service.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
@@ -13,16 +16,25 @@ class SearchView extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 32),
           child: TextField(
-            decoration: InputDecoration(
-              border: const OutlineInputBorder(),
-              label: const Text("Search"),
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              label: Text("Search"),
               hintText: "Enter City Name",
-              suffixIcon: IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.search_outlined),
+              suffixIcon: Icon(
+                (Icons.search_outlined),
               ),
             ),
-            onSubmitted: (value) {},
+            onSubmitted: (value) async {
+              if (value.trim().isNotEmpty) {
+                try {
+                  WeatherModel weatherModel =
+                      await WeatherService().getWeather(cityName: value.trim());
+                  log(weatherModel.cityName.toString());
+                } on Exception {
+                  rethrow;
+                }
+              }
+            },
           ),
         ),
       ),
